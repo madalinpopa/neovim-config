7e93e291f9dab11a7fddc7ae5b266700c9d8311d
Madalin Popa <contact@madalinpopa.com>
2024-09-12 17:23:47 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,8 @@
 require("coderustle")
+
+vim.cmd [[
+  autocmd BufWritePre *.py lua vim.lsp.buf.format()
+  autocmd BufWritePre *.go lua vim.lsp.buf.format()
+  autocmd BufWritePre *.lua lua vim.lsp.buf.format()
+]]
+
